6b3b148e75eead8e7c26715bcee0d81bf2ca9cc9
WAYNE GU <wayne92gu@gmail.com>
2015-02-02 18:08:46 +0800

    update time 2015/2/2

--- crawler.py
+++ crawler.py
@@ -7,19 +7,24 @@ def crawler():
     import bs4
     import re
 
-    url = r"http://item.taobao.com/item.htm?spm=a219r.lm893.14.1.A1oXVV&id=41171239568&ns=1&abbucket=3#detail"
+    url = r"http://item.taobao.com/item.htm?spm=a219r.lm893.14.38.A1oXVV&id=41239809200&ns=1&abbucket=3#detail"
     h = httplib2.Http()
     response, content = h.request(url)
     soup = bs4.BeautifulSoup(content)
     # open(r'C:\Users\怡炜\Desktop\content.txt', 'w').write(content.decode("gbk"))
-    content_str = content.decode('gbk')
-    microscope_data = re.search(r'pageId=(\d+);prototypeId=(\d+);siteId=(\d+); shopId=(\d+); userid=(\d+)', content_str)
-    print(microscope_data)
-    meta = soup.meta
-    a = meta.find(attrs={'name': 'microscope-data'})
-    # print(soup.findAll(name="meta"))
-    print(a)
-    # print(response, content)
+    ids = soup.meta.find(attrs={'name': 'microscope-data'}).attrs['content']
+    splitid = re.split(';', ids)
+    pageId = re.split('=', splitid[0])[1]
+    prototypeId = re.split('=', splitid[1])[1]
+    siteId = re.split('=', splitid[2])[1]
+    shopId = re.split('=', splitid[3])[1]
+    userid = re.split('=', splitid[4])[1]
+    print('pageId='+pageId)
+    print('prototypeId='+prototypeId)
+    print('siteId='+siteId)
+    print('shopId='+shopId)
+    print('userid='+userid)
+    print(ids)
 
 
 if __name__ == "__main__":

81282b8a8893a10a5f7fe456120c78ad3b0e27e8
WAYNE GU <wayne92gu@gmail.com>
2015-02-04 14:32:53 +0800

    update time 2015-02-04 14:32:00 fix gbk decoding problem todo: banned by host for too many requests

--- crawler.py
+++ crawler.py
@@ -31,11 +31,12 @@ def generate_id(url):
     return {'userid': userid, 'auctionId': auctionId, 'siteId': siteId}
 
 
-def crawler():
-    url = r"http://item.taobao.com/item.htm?spm=a219r.lm893.14.35.YyNnUx&id=41239809200&ns=1&abbucket=3#detail"
+def crawler(url):
     ids = generate_id(url)
     comments = open(r'C:\Users\怡炜\Desktop\comments.txt', 'w+')
+    headers = {'Referer': url, 'Connection': 'keep-alive', 'Host': 'rate.taobao.com', 'DNT': '1', 'Cookie': '', 'User-Agent': 'Mozilla/5.0'}
     for pageNum in range(1, 1000):
+        print("page %s" % pageNum)
         KsTS = str(int(time()*1000))+'_'+str(random.randint(1000, 9999))
         rateUrl = 'http://rate.taobao.com/feedRateList.htm?_ksTS=%(KsTS)s&callback=jsonp_reviews_list&' \
                   'userNumId=%(userid)s&auctionNumId=%(auctionNumId)s&siteID=%(siteId)s&currentPageNum=%(pageNum)s&' \
@@ -44,15 +45,17 @@ def crawler():
                      'siteId': ids['siteId'], 'pageNum': pageNum}
         # print(rateUrl)
         h = httplib2.Http()
-        response, content = h.request(rateUrl)
+        response, content = h.request(rateUrl, headers=headers)
+        headers['Cookie'] = response['set-cookie']
         # open(r'C:\Users\怡炜\Desktop\content2.txt', 'w').write(content.decode("gbk"))
-        jsonContent = json.loads(content[23:-3].decode("gbk"))
-        for i in range(0, 19):
+        jsonContent = json.loads(content.decode("gbk", "ignore")[23:-3])
+        for i in range(0, 20):
             comment = jsonContent['comments'][i]['content'].replace('<br/>', '').replace('\n', '')
             # print(comment)
             comments.write(comment+'\n'*2)
 if __name__ == "__main__":
+    url = r"http://item.taobao.com/item.htm?spm=a219r.lm893.14.35.YyNnUx&id=41239809200&ns=1&abbucket=3#detail"
     start = time()
-    crawler()
+    crawler(url)
     stop = time()
-    print("processing time: %s" % str(stop-start))
+    print("processing time: %s seconds" % str(stop-start))

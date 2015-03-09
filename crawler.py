70eaa18398b76981f514f1bb2c444fb1e638dfb2
WAYNE GU <wayne92gu@gmail.com>
2015-03-09 20:23:01 +0800

    update time 2015-3-9 20:21:46 Python2.7 code reviews processing integrated

--- crawler.py
+++ crawler.py
@@ -1,18 +1,10 @@
 # -*- coding: utf8 -*-
 __author__ = '怡炜'
 
-
-import re
-import sys
-import gzip
-import json
-import time
-import random
-import urllib2
-import BeautifulSoup
-
-
 def generate_id(url):
+    import re
+    import urllib2
+    import BeautifulSoup
     auctionId = re.search('id=(\d+)', url).group(1)
     content = urllib2.urlopen(url).read()
     soup = BeautifulSoup.BeautifulSoup(content)
@@ -24,11 +16,20 @@ def generate_id(url):
 
 
 def crawler(url):
+    import re
+    import sys
+    import json
+    import time
+    import random
+    import urllib2
+    reload(sys)
+    sys.setdefaultencoding('utf-8')
     ids = generate_id(url)
     comments = open(r'comments_data/comments_%s.txt' % ids['auctionId'], 'w+')
     headers = {'Referer': url, 'Connection': 'keep-alive', 'Host': 'rate.taobao.com', 'User-Agent': 'Mozilla/5.0'}
     pageNum = 1
     review_id = 1
+    current_review_list = []
     segmentor = u'[ ，,.。！!？?：:；、~～="“”（）()/〓【】★☆…\[\]\*]'
     while(1):
         KsTS = str(int(time.time()*1000))+'_'+str(random.randint(1000, 9999))
@@ -40,14 +41,22 @@ def crawler(url):
         # print rateUrl
         opener = urllib2.build_opener()
         req = urllib2.Request(url=rateUrl, headers=headers)
-        content = opener.open(req).read()
+        try:
+            content = opener.open(req, timeout=3).read()
+        except Exception:
+            continue
         jsonContent = json.loads(content.decode("gbk", "ignore")[23:-3])
         if jsonContent['comments']:
             print('page %d' % pageNum)
             for i in range(0, len(jsonContent['comments'])):
-                comment = jsonContent['comments'][i]['content'].replace('<br/>', '').replace('\n', '').replace('&hellip;', '')
+                comment = jsonContent['comments'][i]['content'].replace('<br/>', '').replace('\n', '').replace('&hellip;', ' ')
                 comment = re.sub(segmentor, ' ', comment)
+                comment = re.sub(r' +', ' ', comment)
                 print review_id
+                if len(comment) > 10:
+                    if comment in current_review_list:
+                        continue
+                current_review_list.append(comment)
                 comment = 'id:%d ' % review_id + comment.strip()
                 # print(comment)
                 comments.write(comment)
@@ -63,8 +72,7 @@ def crawler(url):
 
 
 if __name__ == "__main__":
-    reload(sys)
-    sys.setdefaultencoding('utf-8')
+    import time
     url = r"http://item.taobao.com/item.htm?id=42180621470&ns=1&abbucket=0#detail"
     start = time.time()
     crawler(url)
